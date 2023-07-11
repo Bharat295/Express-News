657f9cfd61c243dd465b61c0cb8555a051197b1a
Bharat <bharatlakhera295@gmail.com>
2023-07-11 18:04:04 +0530

    Updates

---
+++ precache-manifest.97364a6e066f8b6c896ef16b2c4cfeb4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ac961d042bd6d260f48",
+    "url": "/Express-News/static/css/main.7b53746f.chunk.css"
+  },
+  {
+    "revision": "5ac961d042bd6d260f48",
+    "url": "/Express-News/static/js/main.17ccdc44.chunk.js"
+  },
+  {
+    "revision": "5babb9b8fc59f76ea04f",
+    "url": "/Express-News/static/js/runtime~main.0f0a5de1.js"
+  },
+  {
+    "revision": "6e10c61b46be6a5cd359",
+    "url": "/Express-News/static/js/2.3590861a.chunk.js"
+  },
+  {
+    "revision": "5e1c4a9743c24123230f",
+    "url": "/Express-News/static/js/3.02c98a0d.chunk.js"
+  },
+  {
+    "revision": "444c0d9cf29d7ea460bff38539512cab",
+    "url": "/Express-News/static/media/loading.444c0d9c.gif"
+  },
+  {
+    "revision": "58b1f679883bfb1c502d83be881e65da",
+    "url": "/Express-News/index.html"
+  }
+];
